6f05ec606afed7f8d5e0f458edd05eb0437a0996
TAT0426 <69855653+TAT0426@users.noreply.github.com>
2020-08-19 10:08:55 +0800

    Update PrintAndHide.cs

--- Assets/Scenes/PrintAndHide.cs
+++ Assets/Scenes/PrintAndHide.cs
@@ -7,9 +7,10 @@ public class PrintAndHide : MonoBehaviour
     // Start is called before the first frame update
     public int i = 0;
     public Renderer rend;
+    public int j;
     void Start()
     {
-        
+        j = Random.Range(200, 251);
     }
 
     // Update is called once per frame
@@ -17,5 +18,13 @@ public class PrintAndHide : MonoBehaviour
     {
         Debug.Log(gameObject+": "+ i);
         i++;
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+        if (gameObject.tag == "Blue" && i == j)
+        {
+            rend.enabled = false;
+        }
     }
 }
